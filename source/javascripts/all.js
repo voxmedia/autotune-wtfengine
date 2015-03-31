4254c011869e9aff2bd2f18044ee865204f39aff
Ryan Mark <ryan@mrk.cc>
2015-03-31 17:03:54 -0400

    adjust data to reflect what the alpaca form will produce

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -5,6 +5,11 @@ $(function() {
     var pymChild = pym.Child();
     // Anytime you change the height of the graphic, do this:
     // pymChild.sendHeight();
+    var data = {heading: AUTOTUNE.heading, response: AUTOTUNE.response, template: AUTOTUNE.template};
+
+    for (var prop in AUTOTUNE.words) {
+      data[prop] = AUTOTUNE[prop];
+    }
     WTF.init(AUTOTUNE);
   });
 });
