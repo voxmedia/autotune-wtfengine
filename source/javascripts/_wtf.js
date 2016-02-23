340fe4c2b485e99df212e6fa8ba9a31b0d731280
caseymm <casey.miller@voxmedia.com>
2016-02-23 14:54:54 -0500

    Don't let \n break the project

--- source/javascripts/_wtf.js
+++ source/javascripts/_wtf.js
@@ -33,8 +33,13 @@ var WTF = (function() {
     */
 
     function start() {
-        
+
         // Copy out templates then remove from corpus
+        $.each(corpus.template, function(i, d){
+          if(d === ""){
+            corpus.template.splice(i, 1);
+          }
+        })
 
         templates = corpus.template;
         responses = corpus.response;
@@ -204,7 +209,7 @@ var WTF = (function() {
         for ( var key in corpus )
 
             copy[ key ] = corpus[ key ].concat();
-        
+
         return copy;
     }
 
@@ -223,7 +228,7 @@ var WTF = (function() {
             Expects one of the following:
 
                 1.  An object with `templates` and any amount of keys for word types, for example:
-        
+
                     {
                         templates: [ 'The @color @animal', 'The @animal was @color' ],
                         animal: [ 'dog', 'cat', 'rabbit' ],
@@ -233,7 +238,7 @@ var WTF = (function() {
                 2.  A path to a JSON file with the same structure as above (see `sample.json`)
 
                 3.  A Google spreadsheet key (e.g 0AvG1Hx204EyydF9ub1M2cVJ3Z1VGdDhTSWg0ZV9LNGc)
-                    You must first publish the spreadsheet as a CSV 
+                    You must first publish the spreadsheet as a CSV
                     @see https://support.google.com/drive/answer/37579?hl=en
 
         */
